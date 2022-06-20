06c4aaf29271594da87aed1d8e939361c2c64e1c
Terence Holmes <piece4ulmind@gmail.com>
2022-06-19 23:21:07 -0400

    Completed assignment

---
+++ src/components/Comment.js
@@ -0,0 +1,14 @@
+
+
+function Comment({user,comment}) {
+   
+   
+    return (
+        <li>
+            <h2>{user}</h2>
+            <p>{comment}</p>
+        </li>
+    )  
+}
+  
+  export default Comment;
